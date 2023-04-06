8e61a48e9aa7f0d52e95ad8d9428a727dcaef558
ST10343104 <ST10343104@vcconnect.edu.za>
2023-04-06 11:01:18 +0200

    Moved existing files to a package

---
+++ src/Login.java
@@ -0,0 +1,60 @@
+package src;
+
+import java.util.regex.*;
+
+public class Login {
+
+
+    public boolean checkUserName(User user) {
+        boolean lessThan5 = user.getUserName().length()<=5;
+        boolean containsUnderscore = user.getUserName().contains("_");
+        return lessThan5 && containsUnderscore;
+    }
+    public boolean checkPasswordComplexity(User user) {
+
+        Pattern patternCapital = Pattern.compile("[A-Z]");
+        Matcher matcherCapital = patternCapital.matcher(user.getPassword());
+
+        Pattern patternNumber = Pattern.compile("[0-9]");
+        Matcher matcherNumber = patternNumber.matcher(user.getPassword());
+
+        Pattern patternSpecial = Pattern.compile("[^a-zA-Z\\d\\s:]");
+        Matcher matcherSpecial = patternSpecial.matcher(user.getPassword());
+
+        boolean moreThan8 = user.getPassword().length()>=8;
+        boolean containsCapital = matcherCapital.find();
+        boolean containsNumber = matcherNumber.find();
+        boolean containsSpecial = matcherSpecial.find();
+        return moreThan8 && containsCapital && containsNumber && containsSpecial;
+    }
+
+    public String registerUser(User user) {
+        boolean goodUsername = checkUserName(user);
+        boolean goodPassword = checkPasswordComplexity(user);
+        String response = "";
+        if(goodUsername) {
+            response += "Username successfully captured";
+        } else {
+            response += "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length .";
+        }
+        response += "\n";
+        if(goodPassword) {
+            response += "Password successfully captured";
+        } else {
+            response += "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        }
+        return response;
+    }
+
+    public boolean loginUser(User providedUser, User referenceUser) {
+        return providedUser.equals(referenceUser);
+    }
+
+    public String returnLoginStatus(User providedUser, User referenceUser) {
+        if(loginUser(providedUser, referenceUser)) {
+            return "Welcome " + referenceUser.getFirstName() + " " + referenceUser.getLastName() + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again";
+        }
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,77 @@
+package src;
+
+import java.util.Objects;
+
+public class User {
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String userName, String password, String firstName, String lastName) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userName, user.userName) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, password);
+    }
+
+    @Override
+    public String toString() {
+        return "src.User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
